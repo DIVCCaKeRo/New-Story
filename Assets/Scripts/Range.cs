41f2994fa4cf689447307400b9773a7277777d82
Kevin898y <kevin888y@gmail.com>
2016-10-02 22:48:25 +0800

    New Story

---
+++ Assets/Scripts/Range.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Range : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject explosion, temp;
+    private float now, last;
+    void Start () {
+        now = 0; last = -6.0f;
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("987654321");
+            Vuforia.DefaultTrackableEventHandler.Ready = true;
+        }
+        Vector3 Pos = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(Mathf.Clamp(Pos.x, -8.0f, 8.0f), Mathf.Clamp(Pos.y, -1.0f, 18.0f), Mathf.Clamp(Pos.z,-15.0f ,0.0f ));             
+    }
+    void OnCollisionEnter(Collision test) 
+    {
+        now = Time.time;
+        print(now);
+        if (test.gameObject.name == "Cylinder") 
+        {
+            if(now-last>=5.0f)
+               temp = (GameObject)Instantiate(explosion, transform.position, Quaternion.identity);
+               
+        }
+    }
+    void OnCollisionExit(Collision aaa)
+    {
+        last = Time.time;
+        if (aaa.gameObject.name == "Cylinder")
+        {
+            Renderer renderer = aaa.gameObject.GetComponent<Renderer>();
+            renderer.material.color = Color.green;
+            Destroy(temp);
+        }
+
+    }
+    void OnCollisionStay(Collision aaa)
+    {
+        if (aaa.gameObject.name == "Cylinder")
+        {
+            Renderer renderer = aaa.gameObject.GetComponent<Renderer>();
+            renderer.material.color = Color.yellow;
+        }
+    }
+    
+}
